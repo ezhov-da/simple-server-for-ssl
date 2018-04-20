64ab594f531e479c620486d1f908fade876d78c3
ezhov_da <ezhdenis@yandex.ru>
2018-04-20 21:34:56 +0300

    Добавил информации

--- src/main/java/ru/test/ezhov/App.java
+++ src/main/java/ru/test/ezhov/App.java
@@ -6,6 +6,8 @@ import static spark.Spark.*;
  * Это самое простое приложение предназначенное для получения SSL сертификата от Let’s Encrypt
  * https://zerossl.com/
  * Для импорта сертификата используется keystore-explorer
+ *
+ * Тушим Tomcat и запускаем этот сервер на 8080
  */
 public class App {
     public static void main(String[] args) {
